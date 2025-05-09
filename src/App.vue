67e47899561301bf59a8d212ea81cb78ab622eeb
RickyBv1 <rickycortes668668@gmail.com>
2025-05-09 18:57:09 -0400

    feat: add ClaseTeleports component and integrate with App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script>
 import ClaseTransitions from './components/ClaseTransitions/ClaseTransitions.vue'
+import ClaseTeleports from './components/ClaseTeleports/ClaseTeleports.vue'
 
 export default {
   name: 'App',
-  components: { ClaseTransitions },
+  components: { ClaseTransitions, ClaseTeleports },
 }
 </script>
 
 <template>
-  <ClaseTransitions />
+  <ClaseTransitions class="card" />
+
+  <ClaseTeleports class="card" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  margin-top: 2rem;
+  border: 1px dotted white;
+}
+</style>
---
+++ src/components/ClaseTeleports/ClaseTeleports.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Modal />
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'ClaseTeleports',
+  components: { Modal },
+}
+</script>
