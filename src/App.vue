e8fd419cc7ba22dc3599ebb0c9e8161567cec4ad
RickyBv1 <rickycortes668668@gmail.com>
2025-05-09 21:36:01 -0400

    feat: add CompositionAPI component and integrate into App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,43 @@ import ClaseTransitions from './components/ClaseTransitions/ClaseTransitions.vue
 import ClaseTeleports from './components/ClaseTeleports/ClaseTeleports.vue'
 import ClaseCicloDeVida from './components/ClaseCicloDeVida/ClaseCicloDeVida.vue'
 import ClaseMixins from './components/ClaseMixins/ClaseMixins.vue'
+import CompositionAPI from './compositions/CompositionAPI/CompositionAPI.vue'
 
 export default {
   name: 'App',
-  components: { ClaseTransitions, ClaseTeleports, ClaseCicloDeVida, ClaseMixins },
+  components: { ClaseTransitions, ClaseTeleports, ClaseCicloDeVida, ClaseMixins, CompositionAPI },
 }
 </script>
 
 <template>
-  <ClaseTransitions class="card" />
+  <div id="components">
+    <ClaseTransitions class="card" />
 
-  <ClaseTeleports class="card" />
+    <ClaseTeleports class="card" />
 
-  <ClaseCicloDeVida class="card" />
+    <ClaseCicloDeVida class="card" />
 
-  <ClaseMixins />
+    <ClaseMixins />
+  </div>
+
+  <div id="composition">
+    <CompositionAPI class="card" />
+  </div>
 </template>
 
 <style>
+#components {
+  margin: 1rem 0;
+  border: 1px solid royalblue;
+}
+
 .card {
-  margin-top: 2rem;
+  margin: 1rem 0;
   border: 1px dotted white;
+}
+
+#composition {
+  margin: 1rem 0;
+  border: 1px solid rosybrown;
 }
 </style>
---
+++ src/compositions/CompositionAPI/CompositionAPI.vue
@@ -0,0 +1,12 @@
+<template>
+  <UserRepositories :user="'Ricky'"></UserRepositories>
+</template>
+
+<script>
+import UserRepositories from './components/UserRepositories.vue'
+
+export default {
+  name: 'CompositionAPI',
+  components: { UserRepositories },
+}
+</script>
